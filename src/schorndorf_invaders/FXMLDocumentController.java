e1d7bc8ee3e5c7027944c3d06ff766576ade6228
leon <leon.trogrlic2@gmail.com>
2024-04-12 12:01:58 +0200

    TEST 1

--- src/schorndorf_invaders/FXMLDocumentController.java
+++ src/schorndorf_invaders/FXMLDocumentController.java
@@ -22,7 +22,7 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        System.out.println("TEST GITHUB");
         label.setText("Hello World!");
     }
     
